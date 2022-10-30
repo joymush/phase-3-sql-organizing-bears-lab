3d4c4bbdae996a701c2154ce382c4b4412d2483c
joymush <joy.chemutai@student.moringaschool.com>
2022-10-30 20:23:43 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Mr. Chocolate", 5 ,"M", "Masculine" , "cunning" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Rowdy", 6 ,"F", "Girly" , "Sweet" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Tabitha", 5 ,"F", "Black" , "Sad" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Sergeant Brown", 3 ,"M", "Fruity" , "Amaazing" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Melissa", 5 ,"F", "Orange" , "Clumsy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Grinch", 5 ,"M", "Blonde" , "Sad" , true);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ("Wendy", 7 ,"F", "Green" , "Happy" , false);
+INSERT INTO bears ( name ,age , sex ,color ,temperament ,alive ) VALUES ( NULL,6 ,"M", "Yellow" , "Naughty" , true);
